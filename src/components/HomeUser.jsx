2972c7e8fc28e3bf5ee15cee0f3d17590b9cf5d6
boanit <38143306+boanit@users.noreply.github.com>
2021-01-18 21:49:08 +0900

    Update HomeUser.jsx

--- src/components/HomeUser.jsx
+++ src/components/HomeUser.jsx
@@ -43,12 +43,12 @@ export default function HomeUser() {
 						<p className="home-user-image__desc">boanit is a company that specializes in information security.
 						</p>
 
-						<div className="resume-container">
+						<!--div className="resume-container">
 							<a href="#" download>
 								<i className="ti-download" />
 								<span></span>
 							</a>
-						</div>
+						</div-->
 
 						<SocialLinks className="home-user-image__socials" />
 					</div>
